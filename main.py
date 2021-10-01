cef149158289cacd1c848cc83152c8927b4069dd
MasonN808 <mason.nakamura1@marist.edu>
2021-09-30 23:44:25 -0400

    Optimize speed

--- main.py
+++ main.py
@@ -117,68 +117,14 @@ if __name__ == '__main__':
 
     # Import Default dataset with pandas from downloaded csv file from R
     df = pd.read_csv(r'C:/Users/Mason/Desktop/Default.csv')
-    df = df[["balance", "income"]]
-    df["instances"] = np.ones(len(df.index))
+
+    df['default'] = df['default'].map({'Yes':1, 'No':0})
     print(df)
-    print(df["income"].dtype)
+    df["instances"] = np.ones(len(df.index))
+    df = df[["income", "default", "instances"]]
+    print(df)
+    # print(df["income"].dtype)
 
     model = MLE(data=df, iterations=100)
     print(model.get_probabilities())
     print(model.newtowns_method())
-
-    # xdata = [1.6907, 1.7242, 1.7552, 1.7842, 1.8113, 1.8369, 1.8610, 1.8839]
-    # ydata = [6, 13, 18, 28, 52, 53, 61, 60]
-    # ndata = [59, 60, 62, 56, 63, 59, 62, 60]
-    # data = [xdata, ydata, ndata]
-    # theta1 = syp.Symbol("theta1")
-    # theta2 = syp.Symbol("theta2")
-    # x = syp.Symbol("x")
-    # y = syp.Symbol("y")
-    # params = [theta1, theta2]
-    # # if xdata.size != ndata.size != ydata.size:
-    # #     logging.warning("xdata, ndata, and ydata are not of the same size")
-    # seq = np.arange(len(xdata))
-    # likelihood = 1
-    # pi = []
-    # for i in seq:
-    #     # Create the pi_i's
-    #     pi.append((syp.exp(theta1 + theta2 * data[0][i])) / (1 + syp.exp(theta1 + theta2 * data[0][i])))
-    # print("PI_i: " + str(pi))
-    # for i in seq:
-    #     # Compute the likelihood function
-    #     likelihood *= comb(data[2][i], data[1][i]) * (pi[i] ** data[1][i]) * (1 - pi[i]) ** (
-    #                 data[2][i] - data[1][i])
-    # print("LIKELIHOOD FUNCTION: " + str(likelihood))
-    # # print("DATA: " + str(data))
-    # # Get the log-likelihood function
-    # lnl = syp.ln(likelihood)
-    # # Get the score functions
-    # score1 = syp.diff(lnl, theta1)
-    # score2 = syp.diff(lnl, theta2)
-    # print("Score Function wrt THETA1: " + str(score1))
-    # print("Score Function wrt THETA2: " + str(score2))
-    # # info_matrix = np.matrix([[score1 ** 2, score1 * score2], [score2 * score1, score2 ** 2]])
-    # # info_matrix = np.matrix([[lnl.diff(theta1).diff(theta2) for x in params] for y in params])
-    # info_matrix = syp.Matrix(syp.hessian(lnl, params))
-    # # info_matrix = np.matrix([[lnl.diff(params[0])], lnl.diff[params[1]]])
-    # # print("INFO_MATRIX" + str(info_matrix))
-    #
-    # print("INFO_MATRIX_SHAPE: " + str(info_matrix.shape))
-    # initial = [0, 0]
-    # nxt = None
-    # for i in np.arange(100):
-    #     print(i)
-    #     # information matrix subbed in with values for theta1 and theta2, then converted to float for consistency
-    #     # subed_info_matrix = np.vectorize(lambda z: z.subs({theta1: initial[0], theta2: initial[1]}))(
-    #     #     info_matrix).astype(dtype=np.float64)
-    #     subed_info_matrix = np.matrix(info_matrix.subs(theta1, initial[0]).subs(theta2, initial[1]))
-    #     print(subed_info_matrix)
-    #     scores_array = [score1.subs(theta1, initial[0]).subs(theta2, initial[1]),
-    #          score2.subs(theta1, initial[0]).subs(theta2, initial[1])]
-    #     # scores_array_transpose = np.transpose(scores_array)
-    #     nxt = initial + np.dot(np.linalg.inv(subed_info_matrix.astype(dtype = np.float64)), scores_array)
-    #     # Convert matrix to array with two elements
-    #     nxt = np.asarray(nxt).flatten()
-    #     print(initial)
-    #     initial = nxt
-    # # MLE(pdf, params, data)
