d6ea16056448980d95630d5cc8f9ef73280eec54
MasonN808 <mason.nakamura1@marist.edu>
2021-11-03 00:18:35 -0400

    -Added Synthetic Data -Working on Implementation -need work on the dot prodoct between thetas and phi (is phi a vector or matrix) (what is phi?) Line 3

--- Stochastic_Newton_Algo.r
+++ Stochastic_Newton_Algo.r
@@ -1,3 +1,21 @@
-def stochastic_newton_algo(){
-  
+stochastic_newton_algo <- function(df, iterations = 10){
+  thetas <- rep(1, ncols(df)) #initialize vector of thetas assigned 1
+  exponent <- thetas %*%..................
 }
+
+rm(list=ls())
+library(optimx)
+#set.seed(123)
+p <- 5
+n <- 10000
+x <- matrix(rnorm(n * p), n, p)
+x=cbind(1,x)
+betas <- runif(p+1, -2, 2)
+hc <- function(x) 1 /(1 + exp(-x)) # inverse canonical link
+p.true <- hc(x %*% betas)
+y <- rbinom(n, 1, p.true)
+df <- cbind(x,y)
+head(df)
+
+stochastic_newton_algo(df, 10)
+
