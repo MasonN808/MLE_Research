0dc6306cfad0e2e7bcc599b3bb4153cd0a0bce30
MasonN808 <mason.nakamura1@marist.edu>
2021-11-08 23:18:38 -0500

    Edits to Stochastic newton algorithm

--- Stochastic_Newton_Algo.r
+++ Stochastic_Newton_Algo.r
@@ -4,7 +4,7 @@ stochastic_newton_algo <- function(df, iterations = 10){
                               #number of columns from df
   S_n_inv_prev = diag(ncol(df)-1)
   for(i in 1:iterations){
-    PHI <- df[i %% iterations, 0:(ncol(df)-1)] #take the ith row in df for every
+    PHI <- df[i %% nrow(df), 0:(ncol(df)-1)] #take the ith row in df for every
                                               #instance in the sequence, if
                                               #i>iterations, use mod function
                                               #and go through df again until
@@ -19,15 +19,16 @@ stochastic_newton_algo <- function(df, iterations = 10){
     S_n_inv <- S_n_inv_prev - a_n * 1/(1 + a_n*drop((t(PHI) %*% S_n_inv_prev %*% matrix(PHI)))) * 
       S_n_inv_prev %*% matrix(PHI) %*% t(PHI) %*% S_n_inv_prev
     
-    print(thetas_prev)
-    print(dim(S_n_inv %*% PHI))
-    print(dim(drop((df[i %% iterations, ncol(df)] - PI))))
-    print(dim(PI))
-    print(dim(df[i %% iterations, ncol(df)]))
+    #Debugging
+    #print(dim(drop(thetas_prev)))
+    #print(dim(S_n_inv %*% PHI))
+    #print(dim(drop((df[i %% iterations, ncol(df)] - PI))))
+    #print(dim(PI))
+    #print(dim(df[i %% iterations, ncol(df)]))
     
-    thetas <- thetas_prev + (S_n_inv %*% PHI) * drop((df[i %% iterations, ncol(df)] - PI))
+    thetas <- thetas_prev + (S_n_inv %*% PHI) * drop((df[i %% nrow(df), ncol(df)] - PI))
     
-    thetas_prev <- thetas #unnecessary, but good for clarity
+    thetas_prev <- drop(thetas) #Use drop to drop extra dimension from thetas
     S_n_inv_prev <- S_n_inv
     print(thetas_prev)
   }
@@ -48,5 +49,5 @@ y <- rbinom(n, 1, p.true)
 df <- cbind(x,y)
 head(df)
 
-stochastic_newton_algo(df, 10)
+stochastic_newton_algo(df, 100)
 
