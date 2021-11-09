dd6958dbc9dbfe8954772d8769f7ef3397c13348
MasonN808 <mason.nakamura1@marist.edu>
2021-11-08 21:22:54 -0500

    -Working on stochastic newton algorithm implementation -Problems with dot product of PHI and S_n_inv

--- Stochastic_Newton_Algo.r
+++ Stochastic_Newton_Algo.r
@@ -1,6 +1,26 @@
 stochastic_newton_algo <- function(df, iterations = 10){
-  thetas <- rep(1, ncols(df)) #initialize vector of thetas assigned 1
-  exponent <- thetas %*%..................
+  thetas_prev <- rep(1, ncol(df)) #initialize vector of thetas assigned 1 (This can 
+                              #be changed upon initiation) with length of 
+                              #number of columns from df
+  S_n_inv_prev = diag(ncol(df)-1)
+  for(i in 0:iterations){
+    PHI <- df[i %% iterations, 0:(ncol(df)-1)] #take the ith row in df for every
+                                              #instance in the sequence, if
+                                              #i>iterations, use mod function
+                                              #and go through df again until
+                                              #termination
+    exponent <- thetas_prev %*% PHI #calculate the exponent of the logistic function
+    PI <- exp(exponent)/(1+exp(exponent)) #logistic function
+    a_n <- PI*(1-PI)
+    print(dim(PHI))
+    print(dim(S_n_inv))
+    S_n_inv <- S_n_inv_prev - a_n*solve(1+a_n*(t(PHI) %*% S_n_inv_prev %*% PHI))*S_n_inv_prev %*%
+      PHI %*% t(PHI) %*% S_n_inv_prev
+    thetas <- thetas_prev + S_n_inv %*% PHI*(df[i %% iterations, ncol(df)] - PI)
+    thetas_prev <- thetas #unnecessary, but good for clarity
+    print(thetas_prev)
+  }
+
 }
 
 rm(list=ls())
@@ -16,6 +36,6 @@ p.true <- hc(x %*% betas)
 y <- rbinom(n, 1, p.true)
 df <- cbind(x,y)
 head(df)
-s
+
 stochastic_newton_algo(df, 10)
 
