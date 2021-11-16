e24f9a9d314073cd20af12d8591270fb67672063
MasonN808 <mason.nakamura1@marist.edu>
2021-11-15 21:08:22 -0500

    Added documentation comments and testing.

--- trunc_stochastic_newton_algo.R
+++ trunc_stochastic_newton_algo.R
@@ -1,3 +1,10 @@
+#' Implementation of the truncated stochastic Newton algorithm
+#' 
+#' @param df A data frame.
+#' @param thetas_prev A vector of initial theta values in the Reals.
+#' @param CONSTANT A positive real number.
+#' @param BETA A number in [0, 1/2].
+#' @return A data frame of thetas after nrow(df) iterations.
 trunc_stochastic_newton_algo <- function(df, thetas_prev = rep(1, ncol(df)-1), CONSTANT, BETA){
   # initialize a data frame to store thetas after every iteration
   m <- matrix(NA, ncol = ncol(df), nrow = 1)
@@ -35,3 +42,18 @@ trunc_stochastic_newton_algo <- function(df, thetas_prev = rep(1, ncol(df)-1), C
   }
   return(temp_df)
 }
+
+# Testing
+p <- 5
+n <- 10000
+x <- matrix(rnorm(n * p), n, p)
+x=cbind(1,x)
+betas <- runif(p+1, -2, 2)
+hc <- function(x) 1 /(1 + exp(-x)) # inverse canonical link
+p.true <- hc(x %*% betas)
+y <- rbinom(n, 1, p.true)
+df <- cbind(x,y)
+head(df)
+init=betas+rnorm(p+1,0,1)
+
+print(head(trunc_stochastic_newton_algo(df,init, 1/4, 1/2)))
