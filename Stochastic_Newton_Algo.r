150a45703a5e0e02275e58ba78b38f6dce0d123d
MasonN808 <mason.nakamura1@marist.edu>
2021-11-15 20:44:35 -0500

    Made stochastic_newton_algo output a data frame of thetas.

--- Stochastic_Newton_Algo.r
+++ Stochastic_Newton_Algo.r
@@ -1,39 +1,36 @@
 stochastic_newton_algo <- function(df, thetas_prev = rep(1, ncol(df)-1)){
-  #thetas_prev <- rep(1, ncol(df)-1) #initialize vector of thetas assigned 1 (This can 
-  #be changed upon initiation) with length of 
-  #number of columns from df
+  # initialize a data frame to store thetas after every iteration
+  m <- matrix(NA, ncol = ncol(df), nrow = 1)
+  temp_df <- data.frame(m)
+  
+  names <- c()
+  # make a vector of names for columns
+  for (i in 1:ncol(df)-1) {
+    names <- append(names, paste0("Theta.", i))
+  }
+  # assign the names vector to the column names of temp data frame
+  colnames(temp_df) <- names
+  # insert first initial thetas from input and remove NAs when initiated temp_df(could be done differently to name columns?)
+  temp_df <- na.omit(rbind(temp_df, thetas_prev))
   iterations = nrow(df)
   S_n_inv_prev = diag(ncol(df)-1)
   for(i in 1:iterations){
-    PHI <- df[i, 1:(ncol(df)-1)] #take the ith row in df for every
-    #instance in the sequence, if
-    #i>iterations, use mod function
-    #and go through df again until
-    #termination
-    
+    PHI <- df[i, 1:(ncol(df)-1)] #take the ith row in df for every instance in the sequence
     exponent <- thetas_prev %*% PHI #calculate the exponent of the logistic function
     PI <- exp(exponent)/(1+exp(exponent)) #logistic function
     a_n <- drop(PI*(1-PI)) #Use drop to convert from 1x1 matrix to scalar
     
-    
-    
     S_n_inv <- S_n_inv_prev - a_n * 1/(1 + a_n*drop((t(PHI) %*% S_n_inv_prev %*% matrix(PHI)))) * 
       S_n_inv_prev %*% matrix(PHI) %*% t(PHI) %*% S_n_inv_prev
-    
-    #Debugging
-    #print(dim(drop(thetas_prev)))
-    #print(dim(S_n_inv %*% PHI))
-    #print(dim(drop((df[i %% iterations, ncol(df)] - PI))))
-    #print(dim(PI))
-    #print(dim(df[i %% iterations, ncol(df)]))
     
     thetas <- thetas_prev + (S_n_inv %*% PHI) * drop((df[i %% nrow(df), ncol(df)] - PI))
     
     thetas_prev <- drop(thetas) #Use drop to drop extra dimension from thetas
     S_n_inv_prev <- S_n_inv
-    print(thetas_prev)
+    
+    temp_df <- rbind(temp_df, thetas_prev)
   }
-  
+  return(temp_df)
 }
 
 #rm(list=ls())
@@ -51,4 +48,6 @@ df <- cbind(x,y)
 head(df)
 init=betas+rnorm(p+1,0,1)
 
-stochastic_newton_algo(df,init)
+
+# Testing
+print(head(stochastic_newton_algo(df,init)))
