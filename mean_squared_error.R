ea62ab0835c2b5521354a9f10f08cf398512d45f
MasonN808 <mason.nakamura1@marist.edu>
2022-01-23 19:38:51 -0500

    Initialize mean_squared_error for SGD algo

---
+++ mean_squared_error.R
@@ -0,0 +1,16 @@
+#' Implementation of loss function: MSE (Mean Squared Error)
+#' 
+#' @param prediction A vector of predictions.
+#' @param actual A vector of actual values
+#' @return Real number
+mean_squared_error <- function(prediction, actual){
+  if (length(prediction) != length(actual)){
+    return("Error: Length of prediction vector and actual vector are not of same size")
+  }
+  n = length(prediction)
+  prev = 0  #Initialize loss
+  for (i in 1: n){
+    prev <- prev + (prediction[i] - actual[i])^2
+  }
+  return(prev)
+}
