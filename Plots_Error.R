d61a434d74569390effec1c7d7cd7bc0ce2b403d
MasonN808 <mason.nakamura1@marist.edu>
2022-01-29 21:26:09 -0500

    Added lines to supply SGD with error plot.
    HOWEVER, need to output a data frame from sgd2 with error, similar to other newton algorithms.

--- Plots_Error.R
+++ Plots_Error.R
@@ -1,4 +1,8 @@
 # Testing
+
+library(ggplot2)
+
+#Initialize synthetic data
 p <- 5
 n <- 10000
 x <- matrix(rnorm(n * p), n, p)
@@ -15,13 +19,17 @@ init=betas+rnorm(p+1,0,1)
 # print(tail(stochastic_newton_algo(df,init)))
 output.df <- stochastic_newton_algo(df,init, exact = betas)
 output.df1 <- trunc_stochastic_newton_algo(df,init, exact = betas, 1/4, 1/2) # What do these constants do?
+output.df2 <- sgd2(df)
 ## DELETE LAST LINE OF output.df SINCE ALL 0s (ISSUE)
 output.df <- head(output.df, -1)
 output.df1 <- head(output.df1, -1)
+output.df2 <- head(output.df2, -1)
 
 error <- output.df[, ncol(output.df)]
 
 error1 <- output.df1[, ncol(output.df1)]
+
+error2 <- output.df2[, ncol(output.df2)]
 
 # Make the plots
 n <- seq(1, n, by = 1)
@@ -30,6 +38,8 @@ p <- ggplot()+
   geom_point(aes(n, y = error), color = "forestgreen", shape = 15, size = .25) +
   geom_line(aes(n, y = error1), color = "orange") + 
   geom_point(aes(n, y = error1), color = "orange", shape = 15, size = .25) + 
+  geom_line(aes(n, y = error2), color = "salmon") + 
+  geom_point(aes(n, y = error2), color = "salmon", shape = 15, size = .25) + 
   ggtitle("Error plot")
 print(p)
 
@@ -37,6 +47,8 @@ p <- ggplot() +
   geom_boxplot(aes(x = "SNA", y=error), outlier.colour="red", outlier.shape=8,
                outlier.size=1) + 
   geom_boxplot(aes(x = "TSNA", y=error1), outlier.colour="red", outlier.shape=8,
-               outlier.size=1)
+               outlier.size=1) +
+  geom_boxplot(aes(x = "SGD", y=error1), outlier.colour="red", outlier.shape=8,
+             outlier.size=1)
 print(p)
 
