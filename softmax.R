7b7a9378436bbbd2f62194fce997f03950eedcd6
Mason Nakamura <Mason.Nakamura1@marist.edu>
2022-02-28 10:53:30 -0500

    Added indicator function, adding in gradient

--- softmax.R
+++ softmax.R
@@ -9,22 +9,37 @@ prob_softmax <- function(x, Beta, sum){
   return(out)
 }
 
-
+#' Indicator Function
+#' 
+#' @param y a number
+#' @param c a number from possible target variables
+#' @return 1 or 0
+ind_func <- function(x, Beta, sum){
+  if (y == c){
+    out = 1
+  }
+  else{
+    out = 0
+  }
+  return(out)
+}
 
 #' Implementation of softmax regression
 #' 
 #' @param df A data frame.
 #' @param eta Learning rate
 #' @param num_iter number of iterations
-#' @param batch_num number of data rows in the batch (specifically for SGD)
+#' @param batch_num number of data rows in the subsample
 #' @return theta
 softmax <- function(df, init = as.vector(rep(1, ncol(df)-1)), eta = .001, num_iter = 1000, batch_num = 30, exact = NULL){
   betas = init  # initializing weights
   while(i < num_iter){
     sum = sum(exp(x, betas))
     p.k = prob_softmax(x, beta, sum)
+    delta.i = ind_func(y, c)
     s.i = delta.i - p.k
-    gradient = 1/N*
+    tensor_product = s.i %x% p.k #This will be a K x N matrix
+    gradient = 1/N*sum(tensor_product[1:nrow(tensor_product)])
     i <- i + 1
   }
 }
