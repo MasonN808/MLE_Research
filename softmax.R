03b7d49ed63bb2ddc94981685224e28dafc4835f
MasonN808 <mason.nakamura1@marist.edu>
2022-04-03 22:30:55 -0400

    Outputs B_full successfully

--- softmax.R
+++ softmax.R
@@ -85,44 +85,76 @@ softmax <- function(df, K, init = matrix(1, ncol(df)-1, nrow(df)-1), batch_num =
   Y <- batch[,ncol(df)] # vector of target values
   
   B_full = rep(0, ncol(df)-1)
+  # print(size(B_full))
   
   for(i in 1:batch_num){
     N = batch_num
     k = 1:K
     j = 1:N
 
-    cat(paste("x[i,]: ", x[i,], "\n"))
+    # cat(paste("x[i,]: ", x[i,], "\n"))
     # print(betas)
     # print(betas[k])
     sum = sum(exp(X[i,] %*% betas[,k]))
-
-    for (j in 1:K) {
-      #CONTINUE HERE 3/21 FIX p.k to be a vector
-      p.k = prob_softmax(X[i,], betas[,j], sum) # d dimensional vector
+    
+    p.k.vec = c()
+    s.i.vec = c()
+    for (j in 1:(K+1)) {
+      p.k = prob_softmax(X[i,], betas[,j], sum) # K dimensional vector : betas[,j] d dimesional : X[i,] d dimensional
       # cat(paste("betas[,j]: ", betas[,j], "\n"))
-      cat(paste("p.k: ", p.k, "\n"))
-      
+      # cat(paste("p.k: ", p.k, "\n"))
+      p.k.vec <- c(p.k.vec, p.k)
       delta.i = ind_func(Y[i], j)
-      cat(paste("delta.i: ", delta.i, "\n"))
+      # cat(paste("delta.i: ", delta.i, "\n"))
       
       s.i = delta.i - p.k # K dimensional vector
-      cat(paste("s.i: ", s.i, "\n", "--------------------------", "\n"))
+      # cat(paste("s.i: ", s.i, "\n", "--------------------------", "\n"))
+      s.i.vec <- c(s.i.vec, s.i)
+      # print(p.k.vec)
+      # print(j)
+    }
+    # print(p.k.vec)
+    
+    # for (j in 1:K) {
+      #CONTINUE HERE 3/21 FIX p.k to be a vector
+      # p.k = prob_softmax(X[i,], betas[,j], sum) # K dimensional vector : betas[,j] d dimesional : X[i,] d dimensional
+      # cat(paste("betas[,j]: ", betas[,j], "\n"))
+      # cat(paste("p.k: ", p.k, "\n"))
       
-
+      # delta.i = ind_func(Y[i], j)
+      # cat(paste("delta.i: ", delta.i, "\n"))
       
-      derivative.1 = 1/N*(s.i %x% X[i]) # This will be a K x N matrix, %x% := tensor product
+      # s.i = delta.i - p.k # K dimensional vector
+      # cat(paste("s.i: ", s.i, "\n", "--------------------------", "\n"))
+      
+      derivative.1 = 1/N*(s.i.vec %x% t(X[i,])) # This will be a K x d matrix, %x% := tensor product
       # cat(paste("derivative.1: ", derivative.1, "\n", "--------------------------", "\n"))
+      # print(derivative.1)
       
-      phi = p.k - p.k^2 # K by K dimensional matrix
-      # cat(paste("phi: ", phi, "\n", "--------------------------", "\n"))
+      # Populate Phi matrix
+      for(j in 1:(K+1)){
+        for(k in 1: (K+1)){
+          phi = matrix(rep(-p.k.vec[j]*p.k.vec[k], (K+1)*(K+1)), nrow = K+1, byrow = TRUE) # K by K dimensional matrix
+          # cat(paste("phi: ", phi, "\n", "--------------------------", "\n"))
+        }
+      }
+      # print(phi)
+      
+      tempP.k.vec <- p.k.vec
+      
+      diag(phi) <- tempP.k.vec - tempP.k.vec^2
+      
+      # print(phi)
       
       derivative.2 = -1/N*(phi %x% (X[i] %*% X[i]))
       # cat(paste("derivative.2: ", derivative.2, "\n", "--------------------------", "\n"))
       
       B_full = B_full + inv(derivative.2) %*% derivative.1
-    }
+    # }
   }
-  return(B_full)
+  print(size(B_full))
+  # B_full = matrix(B_full, nrow = ncol(df)-1)
+  return(t(B_full))
 }
 
 # Testing
@@ -144,7 +176,8 @@ library(pracma)
 # print(df)
 print(cat("targets: ", targets))
 print(cat("test: ", sum(targets+rnorm(p+1,0,1))))
-print(cat("OUT: ", tail(softmax(df, K, init))))
+# print(cat("OUT: ", (softmax(df, K, init))))
+print(softmax(df, K, init))
 #exact values
 
 
